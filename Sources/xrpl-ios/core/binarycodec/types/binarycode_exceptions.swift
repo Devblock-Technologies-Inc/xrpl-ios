6dc544f563927fff1e51f54d4dbcfe3130e9860f
Vuong Nguyen <vuongnm@devblock.net>
2021-12-29 18:00:58 +0700

    binary codec exceptions

--- Sources/xrpl-ios/core/binarycodec/types/binarycode_exceptions.swift
+++ Sources/xrpl-ios/core/binarycodec/types/binarycode_exceptions.swift
@@ -2,6 +2,9 @@ import Foundation
 
 public enum XRPLBinaryCodeException: XRPLException {
     case binaryParser(String)
+    case binarySerializer(String)
+    case definations(String)
+    case types(String)
 }
 
 extension XRPLBinaryCodeException: LocalizedError {
@@ -9,6 +12,12 @@ extension XRPLBinaryCodeException: LocalizedError {
         switch self {
         case .binaryParser(let message):
             return NSLocalizedString("BinaryParser - \(message)", comment: "")
+        case .binarySerializer(let message):
+            return NSLocalizedString("BinarySerializer - \(message)", comment: "")
+        case .definations(let message):
+            return NSLocalizedString("Difinations - \(message)", comment: "")
+        case .types(let message):
+            return NSLocalizedString("Types - \(message)", comment: "")
         }
     }
 }
