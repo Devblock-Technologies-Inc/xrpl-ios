d0757c63d87862faaa2ff44eb13f0158eddb0a12
Vuong Nguyen <vuongnm@devblock.net>
2022-01-06 19:07:48 +0700

    vector256 type

--- Sources/xrpl-ios/core/binarycodec/types/uint_type.swift
+++ Sources/xrpl-ios/core/binarycodec/types/uint_type.swift
@@ -1,5 +1,23 @@
 import Foundation
 
-//public struct UIntType: SerializedType {
-//    
-//}
+/// Base class for serializing and deserializing unsigned integers.
+/// See `UInt Fields <https://xrpl.org/serialization.html#uint-fields>`
+public class UIntType: SerializedType, Hashable {
+    
+    var value: Int {
+        let bigEndianValue = bytes.withUnsafeBufferPointer { $0.baseAddress!.withMemoryRebound(to: Int.self, capacity: 1, { $0 }) }.pointee
+        return Int(bigEndian: bigEndianValue)
+    }
+    
+    public static func ==(lsh: UIntType, rhs: UIntType) -> Bool {
+        return lsh.value == rhs.value
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(value)
+    }
+    
+    public override func toJSON() throws -> Any {
+        return value
+    }
+}
