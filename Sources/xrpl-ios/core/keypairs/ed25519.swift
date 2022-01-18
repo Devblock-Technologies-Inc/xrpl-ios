00994dd74710a559a09de21051aa44c50916dbf2
Vuong Nguyen <vuongnm@devblock.net>
2022-01-18 19:49:23 +0700

    generate keys use Ed25519 algorithm

--- Sources/xrpl-ios/core/keypairs/ed25519.swift
+++ Sources/xrpl-ios/core/keypairs/ed25519.swift
@@ -9,13 +9,21 @@ public class ED25519Service: CryptoProtocol {
         return false
     }
     
+    /// Derives a key pair in Ed25519 format for use with the XRP Ledger from a seed value.
+    ///
+    /// - Parameters
+    ///     - decodedSeed: The Ed25519 seed to derive a key pair from, as bytes.
+    ///     - isValidator: Whether to derive a validator keypair. However, validator signing keys cannot use Ed25519. (See `#3434 <https://github.com/ripple/rippled/issues/3434>`for more information.)
+    /// - Returns: A (public key, private key) pair derived from the given seed.
+    /// - Throws:`XRPLKeypairsException`: If the keypair is a validator keypair.
     public func deriveKeypair(decodedSeed: [UInt8], isValidator: Bool) throws -> KeyPair {
         let rawPrivateKey = Helpers.getSHA512FirstHalf(decodedSeed)
         let privateKey = try Curve25519.Signing.PrivateKey(rawRepresentation: rawPrivateKey)
+        let privateKeyHex = [UInt8](privateKey.rawRepresentation).toHexString()
         let publicKey = privateKey.publicKey
+        let publicKeyHex = [UInt8](publicKey.rawRepresentation).toHexString()
         
-        
-        return KeyPair(privateKey: "", publicKey: "")
+        return KeyPair(privateKey: "ED" + privateKeyHex.uppercased(), publicKey: "ED" + publicKeyHex.uppercased())
     }
 }
 
