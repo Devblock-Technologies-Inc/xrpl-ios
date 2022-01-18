bf60af56580c844ab5e6048fb3505c11d86c6c24
Vuong Nguyen <vuongnm@devblock.net>
2022-01-18 19:41:30 +0700

    replace Base58 encoding like BitcoinKit

--- Sources/xrpl-ios/core/addresscodec/base58.swift
+++ Sources/xrpl-ios/core/addresscodec/base58.swift
@@ -1,112 +1,173 @@
 import Foundation
-import CommonCrypto
-import BigInt
+import xrpl_private
 
-enum Base58 {
+internal protocol XEncoding {
+    static var baseAlphabets: String { get }
+    static var zeroAlphabet: Character { get }
+    static var base: Int { get }
 
-    /// Includes a 4-byte checksum so that the probability of generating a valid address from random characters is approximately 1 in 2**32
-    private static let checksumLength = 4
-    private static let zero = BigUInt(0)
+    // log(256) / log(base), rounded up
+    static func sizeFromByte(size: Int) -> Int
+    // log(base) / log(256), rounded up
+    static func sizeFromBase(size: Int) -> Int
 
-    /// Encode the given bytes into a Base58Check encoded string.
-    /// - Parameter bytes: The bytes to encode.
-    /// - Parameter alphabet: The dictionary used for XRPL base58 encodings
-    /// - Returns: A base58check encoded string representing the given bytes, or empty if encoding failed.
-    public static func encodeCheck(_ bytes: [UInt8], alphabet: [UInt8]) -> String {
-        let checksum = calculateChecksum(bytes)
-        let checksummedBytes = bytes + checksum
-        return Base58.encode(checksummedBytes, alphabet: alphabet)
+    // Public
+    static func encode(_ bytes: Data) -> String
+    static func decode(_ string: String) -> Data?
+}
+
+// The Base encoding used is home made, and has some differences. Especially,
+// leading zeros are kept as single zeros when conversion happens.
+extension XEncoding {
+    static func convertBytesToBase(_ bytes: Data) -> [UInt8] {
+        var length = 0
+        let size = sizeFromByte(size: bytes.count)
+        var encodedBytes: [UInt8] = Array(repeating: 0, count: size)
+
+        for b in bytes {
+            var carry = Int(b)
+            var i = 0
+            for j in (0...encodedBytes.count - 1).reversed() where carry != 0 || i < length {
+                carry += 256 * Int(encodedBytes[j])
+                encodedBytes[j] = UInt8(carry % base)
+                carry /= base
+                i += 1
+            }
+
+            assert(carry == 0)
+
+            length = i
+        }
+
+        var zerosToRemove = 0
+        for b in encodedBytes {
+            if b != 0 { break }
+            zerosToRemove += 1
+        }
+
+        encodedBytes.removeFirst(zerosToRemove)
+        return encodedBytes
     }
-    
-    /// Decode the given Base58Check encoded string to bytes.
-    /// - Parameters:
-    ///     - input: A base58check encoded input string to decode.
-    ///     - alphabet: The dictionary used for XRPL base58 encodings
-    ///     - prefix: The prefix prepended to the bytestring.
-    /// - Returns: Bytes representing the decoded input, or nil if decoding failed.
-    /// - Throws: `XRPLAddressCodecException`: if decoding failed or the checksum is incorrect
-    public static func decodeCheck(_ input: String, alphabet: [UInt8], prefix: [UInt8] = []) throws -> [UInt8] {
-        guard var decodedChecksummedBytes = Base58.decode(input, alphabet: alphabet) else {
+
+    static func encode(_ bytes: Data) -> String {
+        var bytes = bytes
+        var zerosCount = 0
+
+        for b in bytes {
+            if b != 0 { break }
+            zerosCount += 1
+        }
+
+        bytes.removeFirst(zerosCount)
+
+        let encodedBytes = convertBytesToBase(bytes)
+
+        var str = ""
+        while 0 < zerosCount {
+            str += String(zeroAlphabet)
+            zerosCount -= 1
+        }
+
+        for b in encodedBytes {
+            let index = String.Index(utf16Offset: Int(b), in: baseAlphabets)
+            str += String(baseAlphabets[index])
+        }
+
+        return str
+    }
+
+    static func decode(_ string: String) -> Data? {
+        guard !string.isEmpty else { return nil }
+
+        var zerosCount = 0
+        var length = 0
+        for c in string {
+            if c != zeroAlphabet { break }
+            zerosCount += 1
+        }
+        let size = sizeFromBase(size: string.lengthOfBytes(using: .utf8) - zerosCount)
+        var decodedBytes: [UInt8] = Array(repeating: 0, count: size)
+        for c in string {
+            guard let baseIndex: Int = baseAlphabets.firstIndex(of: c)?.utf16Offset(in: baseAlphabets) else { return nil }
+            var carry = baseIndex
+            var i = 0
+            for j in (0...decodedBytes.count - 1).reversed() where carry != 0 || i < length {
+                carry += base * Int(decodedBytes[j])
+                decodedBytes[j] = UInt8(carry % 256)
+                carry /= 256
+                i += 1
+            }
+
+            assert(carry == 0)
+            length = i
+        }
+
+        // skip leading zeros
+        var zerosToRemove = 0
+
+        for b in decodedBytes {
+            if b != 0 { break }
+            zerosToRemove += 1
+        }
+        decodedBytes.removeFirst(zerosToRemove)
+
+        return Data(repeating: 0, count: zerosCount) + Data(decodedBytes)
+    }
+}
+
+private struct _XBase58: XEncoding {
+    static let baseAlphabets = "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz"
+    static var zeroAlphabet: Character = "r"
+    static var base: Int = 58
+
+    static func sizeFromByte(size: Int) -> Int {
+        return size * 138 / 100 + 1
+    }
+    static func sizeFromBase(size: Int) -> Int {
+        return size * 733 / 1000 + 1
+    }
+}
+
+public struct XBase58 {
+    public static func encode(_ bytes: Data) -> String {
+        return _XBase58.encode(bytes)
+    }
+    public static func decode(_ string: String) -> Data? {
+        return _XBase58.decode(string)
+    }
+}
+
+public struct XBase58Check {
+    /// Encodes the data to XBase58Check encoded string
+    ///
+    /// Puts checksum bytes to the original data and then, encode the combined
+    /// data to XBase58 string.
+    /// ```
+    /// let address = XBase58Check.encode([versionByte] + pubkeyHash)
+    /// ```
+    public static func encode(_ payload: Data) -> String {
+        let checksum: Data = xrpl_private.Hash.sha256sha256(payload).prefix(4)
+        return XBase58.encode(payload + checksum)
+    }
+
+    /// Decode the XBase58 encoded String value to original payload
+    ///
+    /// First validate if checksum bytes are the first 4 bytes of the sha256(sha256(payload)).
+    /// If it's valid, returns the original payload.
+    /// ```
+    /// let payload = try XBase58Check.decode(base58checkText)
+    /// ```
+    public static func decode(_ string: String) throws -> [UInt8] {
+        guard let raw = XBase58.decode(string) else {
             throw XRPLAddressCodecException.decodingFailed("XRPL base58 encodings")
         }
-        
-        decodedChecksummedBytes = prefix + decodedChecksummedBytes
-        
-        let decodedChecksum = decodedChecksummedBytes.suffix(checksumLength)
-        let decodedBytes = decodedChecksummedBytes.prefix(upTo: decodedChecksummedBytes.count - checksumLength)
-        let calculatedChecksum = calculateChecksum([UInt8](decodedBytes))
-        
-        guard decodedChecksum.elementsEqual(calculatedChecksum, by: { $0 == $1 }) else {
+        let checksum = raw.suffix(4)
+        let payload = raw.dropLast(4)
+        let checksumConfirm = xrpl_private.Hash.sha256sha256(payload).prefix(4)
+        guard checksum == checksumConfirm else {
             throw XRPLAddressCodecException.decodingFailed("Checksum incorrect")
         }
-        
-        return Array(decodedBytes)
-    }
-    
-    /// Encode the given bytes to a Base58 encoded string.
-    /// - Parameter bytes: The bytes to encode
-    /// - Parameter alphabet: The dictionary used for XRPL base58 encodings
-    /// - Returns: A base58 encoded string representing the given bytes, or empty if encoding failed
-    public static func encode(_ bytes: [UInt8], alphabet: [UInt8]) -> String {
-        var answer: [UInt8] = []
-        var integerBytes = BigUInt(Data(bytes))
-        let radix = BigUInt(alphabet.count)
 
-        while integerBytes > 0 {
-            let (quotient, remember) = integerBytes.quotientAndRemainder(dividingBy: radix)
-            answer.insert(alphabet[Int(remember)], at: 0)
-            integerBytes = quotient
-        }
-
-        let prefix = Array(bytes.prefix { $0 == 0 }).map { _ in alphabet[0] }
-        answer.insert(contentsOf: prefix, at: 0)
-
-        return String(bytes: answer, encoding: .utf8) ?? ""
-    }
-    
-    /// Decode the given base58 encoded string to bytes.
-    /// - Parameter input: The base58 encoded input string to decode.
-    /// - Parameter alphabet: The dictionary used for XRPL base58 encodings
-    /// - Returns: Bytes representing the decoded input, or nil if decoding fail.
-    public static func decode(_ input: String, alphabet: [UInt8]) -> [UInt8]? {
-        var answer = zero
-        var index = BigUInt(1)
-        let byteString = input.bytes
-        let radix = BigUInt(alphabet.count)
-        
-        for char in byteString.reversed() {
-            guard let alphabetIndex = alphabet.firstIndex(of: char) else { return nil }
-            
-            answer += index * BigUInt(alphabetIndex)
-            index *= radix
-        }
-        
-        let bytes = answer.serialize()
-        return [UInt8](Array(bytes.prefix { $0 == alphabet[0] }) + bytes)
-    }
-
-    /// Calculate a checksum for a given input by hashing twice and then taking the first four bytes.
-    /// - Parameter input: The input bytes.
-    /// - Returns: A byte array representing the checksum of the input bytes.
-    private static func calculateChecksum(_ input: [UInt8]) -> [UInt8] {
-        let hashedData = sha256(input)
-        let doubleHashedData = sha256(hashedData)
-        let doubleHasedArray = Array(doubleHashedData)
-
-        return Array(doubleHasedArray.prefix(checksumLength))
-    }
-
-    /// Create a sha256 hash of the given data.
-    /// - Parameter data: Input data to hash.
-    /// - Returns: A sha256 hash of the input data.
-    private static func sha256(_ data: [UInt8]) -> [UInt8] {
-        guard let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH)) else { return [] }
-        CC_SHA256(
-            (Data(data) as NSData).bytes,
-            CC_LONG(data.count),
-            res.mutableBytes.assumingMemoryBound(to: UInt8.self)
-        )
-
-        return [UInt8](res as Data)
+        return [UInt8](payload)
     }
 }
